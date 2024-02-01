863e1f9322e44de6dc6b2906e5a83d784e792b58
Diego AVZ <124291993+Diego-AVZ@users.noreply.github.com>
2024-02-01 10:42:58 +0100

    Update No_Code_Smart_Contracts.py

--- No_Code_Smart_Contracts.py
+++ No_Code_Smart_Contracts.py
@@ -19,46 +19,74 @@ def ini_contract():
     global code
     contract.delete("1.0", tk.END)
     add_spdx_pragma("LFGCONTRACT")
-    add_var(var_type, var_name.get())
     contract.insert(tk.END, code)
-    code = ""
+    # code = ""
     var_name.delete(0, tk.END)
     
-
 vars = []
-
-def add_var(var_type, var_name):
-    myVar = {
-        "type" : "",
-        "name" : ""
-    }
-    vars.append(myVar)
-    global code
-    code = code + " " + var_type + " " + var_name + ";"
-
-var_name = tk.Entry(app, width=10)
-var_name.place(x=130, y=100)
 
 var_type = ""
 
 def set_address():
     global var_type
-    var_type = "address"
+    var_type = "address "
 
-var_type1 = tk.Button(text = "address", command=set_address)
-var_type1.place(x=190, y = 100)
 
 def set_uint():
     global var_type
-    var_type = "uint"
+    var_type = "uint "
 
-var_type2 = tk.Button(text = "uint", command=set_uint)
-var_type2.place(x=250, y = 100)
 
-contract = tk.Text(app, wrap="word", height=10, width=80, state="normal")
-contract.place(x=50, y=150)
+def new_var():
+    global code
+    global var_type
+    global var_name
+    contract.delete("1.0", tk.END)
+    var_name1 = var_name.get()
+    my_new_var = var_type + var_name1
+    print("new Var is " + var_type + " " + var_name1)
+    code = code + " " + my_new_var + ";"    
+    print("code is " + code)
+    contract.insert(tk.END, code)
+    var_name.delete(0, tk.END)
 
-boton = tk.Button(app, text="Ini Contract", command=ini_contract)
-boton.place(x=10, y=100)
+
+def add_deposit():
+    global code
+    code = code + " " + "uint amountDeposited; function deposit() public payable{amountDeposited = msg.value;}"
+    contract.delete("1.0", tk.END)
+    contract.insert(tk.END, code)
+
+
+
+def close_contract():
+    global code
+    code = code + " }"
+    contract.delete("1.0", tk.END)
+    contract.insert(tk.END, code)
+
+var_name = tk.Entry(app, width=15, font=('Arial', 10))
+var_name.place(x=125, y=65)
+
+var_type1 = tk.Button(app, text = "address", width=7, background='#377af4', foreground="white", font=('Arial', 10), cursor='hand2', command=set_address)
+var_type1.place(x=50, y = 50)
+
+var_type2 = tk.Button(text = "uint",  width=7, background='#377af4', foreground="white", font=('Arial', 10), cursor='hand2', command=set_uint)
+var_type2.place(x=50, y = 80)
+
+add_var_but = tk.Button(app, text= "Add Var",  width=7, background='#377af4', foreground="white", font=('Arial', 10), cursor='hand2', command=new_var)
+add_var_but.place(x=240, y=61)
+
+deposit = tk.Button(app, text="Add Deposit ETH",  width=10, background='#f90f04', foreground="white", font=('Arial', 13), cursor='hand2', command=add_deposit)
+deposit.place(x=50, y=100)
+
+contract = tk.Text(app, wrap="word", height=10, width=85, state="normal")
+contract.place(x=50, y=250)
+
+boton = tk.Button(app, text="Ini Contract",  width=10, background='#377af4', foreground="white", font=('Arial', 13), cursor='hand2', command=ini_contract)
+boton.place(x=5, y=10)
+
+close = tk.Button(app, text="close contract }",  width=10, background='#377af4', foreground="white", font=('Arial', 11), cursor='hand2', command=close_contract)
+close.place(x=5, y=125)
 
 app.mainloop() 
